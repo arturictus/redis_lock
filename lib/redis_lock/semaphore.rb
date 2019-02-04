86c4139e76b43231942d89779deedd20a988cef5
Artur Panach <arturictus@gmail.com>
2019-02-04 16:37:57 +0800

    initial aproach

--- lib/redis_lock/semaphore.rb
+++ lib/redis_lock/semaphore.rb
@@ -1,7 +1,8 @@
 class RedisLock
   class Semaphore < Strategy
-
+    attr_reader :lock_caller
     def call(&block)
+      self.lock_caller = block
       ttl = args[:ttl] || lock.config.default_ttl
       set_opts = args[:set_opts] || {}
       while lock.locked?
@@ -9,6 +10,19 @@ class RedisLock
       end
       lock.set(ttl, set_opts)
       _perform(&block)
+    end
+
+    def safe_lock?
+      return true if lock.value == lock_caller
+      if caller.any? { |f|  f =~ lock.value }
+        false
+      else
+        true
+      end
+    end
+
+    def lock_caller=(block)
+      @lock_caller = block.to_s.split("@").last
     end
 
     def after_perform
